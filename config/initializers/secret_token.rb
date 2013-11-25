e2efa417fa6af8a338ad45e32b279598228c70f2
Zach Latta <zach@zachlatta.com>
2013-11-25 11:14:09 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hera::Application.config.secret_key_base = '79a16675794ad0b99e9f6fe8449b9480a2bcb694461c04ed01385a10b5b3ecf2cfe1abf727669b9d3cff67ee3b40287cd20a2b35082ec40297821b459afba973'
